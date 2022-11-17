351a3377b6ae0205c364cb66b4d19ec07a5e63ef
nboano <nboano@outlook.it>
2022-11-17 21:59:09 +0100

    Aggiunto il file kccompile.bat, per compilare direttamente

---
+++ kccompile.bat
@@ -0,0 +1,7 @@
+@echo off
+set cpath=%1
+set wasmpath=%2
+shift
+shift
+clang --target=wasm32 -Wl,--no-entry -Wl,--export-dynamic -Wl,--allow-undefined -Wl,--lto-O3 -Wl,--import-memory -O3 -flto -nostdlib -fdeclspec -o %wasmpath% %cpath%
+@echo on
