a58d4194c9ede5d8920ab39f8688e88df2be34b0
nboano <nboano@outlook.it>
2022-12-21 22:00:52 +0100

    Aggiunte descrizioni classi File, OpenFileDialog

--- kinderc.hpp
+++ kinderc.hpp
@@ -1317,6 +1317,7 @@ extern "C" void __cxa_free_exception(void* ptr);
 #pragma endregion
 #pragma region FILE MANAGEMENT
 
+	/// @brief This class wraps the JavaScript File object.
 	class File : public object {
 		public:
 		File() {};
@@ -1333,32 +1334,50 @@ extern "C" void __cxa_free_exception(void* ptr);
 		__declspec(property(get=get_Name)) string Name;
 		__declspec(property(get=get_MimeType)) string MimeType;
 		#else
+
+		/// @brief The UNIX timestamp when the file was modified for the last time.
 		unsigned long LastModified;
+
+		/// @brief The size of the file, in bytes.
 		unsigned long Size;
+
+		/// @brief The file name.
 		string Name;
+
+		/// @brief The file MIME type, if available.
 		string MimeType;	
 		#endif
 
+		/// @brief Asynchronously reads the text of the file.
+		/// @param callback A callback for when the file will be read, with a param of type const char* where the text will be stored.
 		void ReadAsTextAsync(void(*callback)(const char* data));
 	};
 
 	class OpenFileDialog {
 		public:
 
+		/// @brief Shows a dialog where the user can select one or more files. This method will work only if called by an action of the user.
 		static void Show();
 
+		/// @brief Resets all the dialog options.
 		static void Reset();
 
+		/// @brief The dialog file filer, ex "text/*", ".txt,.c,.cs"
 		static Property<const char*> Filter;
 
+		/// @brief A boolean that indicates if the user is allowed to select more files.
 		static Property<bool> Multiple;
 
+		/// @brief A boolean that indicates if the user is allowed to select an entire directory.
 		static Property<bool> PickDirectory;
 
+		/// @brief An array of files containing the selected ones.
 		static Property<File*> Files;
 
+		/// @brief An integer indicating the selected files number.
 		static Property<int> FilesNumber;
 
+		/// @brief An event triggered when the user changes his selection.
 		static Property<void(*)(void*)> OnChange;
 
 		private:
