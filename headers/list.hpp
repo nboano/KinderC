e31ec432bd64366ba44297abbcdcc149ced49671
nboano <nboano@outlook.it>
2023-01-07 19:16:39 +0100

    Implementazione List::Add(Args...)

--- headers/list.hpp
+++ headers/list.hpp
@@ -19,6 +19,15 @@ public:
     /// @param element The element to add.
     void Add(T element);
 
+    /// @brief Adds multiple elements to the List. If there's not enough space, doubles the dimension of the list automatically.
+    /// @param element The elements to add.
+    /// @param ...args The elements to add.
+    template<typename... Args>
+    void Add(T element, Args... args) {
+        Add(element);
+        Add(args...);
+    }
+
     /// @brief Gets an element from the list.
     /// @param index The element index.
     /// @return The corresponding element, or (T)0 if it does not exists.
