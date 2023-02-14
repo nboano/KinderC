402292368202b6e3bd38e3fa14f48c6ac36b4cbe
nboano <nboano@outlook.it>
2023-02-14 21:54:52 +0100

    Implementazione funzioni matematiche attraverso le builtin di webassembly

--- code/math.cpp
+++ code/math.cpp
@@ -30,65 +30,24 @@ void srand(unsigned int seed) {
     random_next = seed;
 }
 
-double reduce_angle(double x) {
-    while(x >= M_PI * 2) x -= M_PI * 2; 
-    return x;
-}
-double factorial(double n) {
-    // 0! = 1 so just return it
-    if(n == 0) {
-        return 1;
-    }
-    // Recursively call factorial with n-1 until n == 0
-    return n * (factorial(n - 1));
-}
+double __builtin_pow(double, double);
+double __builtin_sin(double);
+double __builtin_cos(double);
+double __builtin_tan(double);
+double __builtin_sqrt(double);
+
 double pow(double n, double power) {
-    double result = n;
-    for(int i = 1; i < power; i++) {
-        result *= n;
-    }
-    return result;
+    return __builtin_pow(n, power);
 }
 double sin(double n) {
-    n = reduce_angle(n);
-    double result = n;
-    double coefficent = 3.0;
-    for(int i = 0; i < 10; i++) {
-        double pw = pow(n, coefficent);
-        double frac = factorial(coefficent);
-        if(i % 2 == 0) {
-            result = result - (pw/frac);
-        } else {
-            result = result + (pw/frac);
-        }
-        coefficent = coefficent + 2;
-    }
-    return result; 
+    return __builtin_sin(n);
 }
 double cos(double x) {
-    x = reduce_angle(x);
-    return sin(x + M_PI / 2);
+    return __builtin_cos(x);
 }
 double tan(double x) {
-    x = reduce_angle(x);
-    return sin(x) / cos(x);
+    return __builtin_tan(x);
 }
-
-float rsqrt(float number) {
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-
-    x2 = number * 0.5F;
-    y  = number;
-    i  = *(long*) &y;
-    i  = 0x5f3759df - (i >> 1);
-    y  = *(float*)&i;
-
-    for (int j = 0; j < 3; j++) 
-        y *= ( threehalfs - ( x2 * y * y ) ); 
-
-    return y;
+double sqrt(double n) { 
+    return __builtin_sqrt(n);
 }
-
-float sqrt(float n) { return 1 / rsqrt(n);}
