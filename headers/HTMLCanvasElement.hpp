f544d1f2feb59856a17484bb0e3c2832d1221646
nboano <nboano@outlook.it>
2023-02-17 18:07:49 +0100

    Aggiunta DrawLine al canvas

--- headers/HTMLCanvasElement.hpp
+++ headers/HTMLCanvasElement.hpp
@@ -26,6 +26,14 @@ class HTMLCanvasElement : public HTMLElement {
     Property<const char*> LineColor {
         [](void* elm, const char* value) {
             ((HTMLCanvasElement*)elm)->setProperty("ctx.fillStyle", value);
+            ((HTMLCanvasElement*)elm)->setProperty("ctx.strokeStyle", value);
+        }, this
+    };
+
+    /// @brief Sets the current line width of the canvas.
+    Property<int> LineWidth {
+        [](void* elm, int value) {
+            ((HTMLCanvasElement*)elm)->setProperty("ctx.lineWidth", (string)value);
         }, this
     };
 
@@ -36,6 +44,15 @@ class HTMLCanvasElement : public HTMLElement {
     /// @param sizeY The height of the rectangle.
     /// @param color The CSS color of the rectangle (OPTIONAL).
     void DrawRectangle(double x, double y, double sizeX, double sizeY, const char* color = "#000");
+
+    /// @brief Draws a line from a certain position to another.
+    /// @param start_x The start X.
+    /// @param start_y The start Y.
+    /// @param end_x The end X.
+    /// @param end_y The end Y.
+    /// @param size The line size (in px).
+    /// @param color The line CSS color (OPTIONAL).
+    void DrawLine(double start_x, double start_y, double end_x, double end_y, int size, const char* color = "#000");
 
     /// @brief Clears the canvas.
     /// @param color The canvas background color.
@@ -49,6 +66,12 @@ void HTMLCanvasElement::Init(){
 void HTMLCanvasElement::DrawRectangle(double x, double y, double sizeX, double sizeY, const char* color) {
     LineColor = color;
     JavaScript::Eval("%s.querySelectorAll('%s')[%i].ctx.fillRect(%f,%f,%f,%f)", docname, query, cindex, x, y, sizeX, sizeY);
+}
+
+void HTMLCanvasElement::DrawLine(double start_x, double start_y, double end_x, double end_y, int size, const char* color) {
+    LineColor = color;
+    LineWidth = size;
+    JavaScript::Eval("let ctx = %s.querySelectorAll('%s')[%i].ctx;ctx.beginPath();ctx.moveTo(%f,%f);ctx.lineTo(%f,%f);ctx.stroke();", docname, query, cindex, start_x, start_y, end_x, end_y);
 }
 
 void HTMLCanvasElement::Clear(const char* color) {
